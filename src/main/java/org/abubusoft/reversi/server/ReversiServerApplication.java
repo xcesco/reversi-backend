5437df9cd2a766d50d1c37a40d961740dfe062bd
xcesco <xcesco@gmail.com>
2020-06-15 21:52:18 +0200

    first commit

---
+++ src/main/java/org/abubusoft/reversi/server/ReversiServerApplication.java
@@ -0,0 +1,13 @@
+package org.abubusoft.reversi.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/abubusoft/reversi/server/ReversiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abubusoft.reversi.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReversiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
